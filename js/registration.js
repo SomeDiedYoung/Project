d9d9e319ce91d398b6e97861965c7071b0a19fdb
SomeDiedYoung <vladimer.osmanovi@Gau.edu.ge>
2025-01-11 18:14:39 +0400

    Adding new Page , fixes

--- js/registration.js
+++ js/registration.js
@@ -15,13 +15,12 @@ showLogin.addEventListener('click', (e) => {
     loginSection.style.display = 'block';
 });
 
-// Индикатор надёжности пароля
 const passwordInput = document.getElementById('password');
 const strengthIndicator = document.getElementById('strengthIndicator');
 const passwordStrength = document.getElementById('passwordStrength');
 
 passwordInput.addEventListener('focus', () => {
-    passwordStrength.style.display = 'block';  // Показать индикатор при фокусе на поле
+    passwordStrength.style.display = 'block';  
 });
 
 passwordInput.addEventListener('input', () => {
@@ -29,21 +28,20 @@ passwordInput.addEventListener('input', () => {
     let strength = 'None';
     let color = 'black';
 
-    // Простая проверка: слабый, средний, сильный
     if (password.length === 0) {
-        strength = 'None';  // Если пароль пустой
+        strength = 'None'; 
         color = 'black';
     } else if (password.length < 6) {
-        strength = 'Weak';  // Если пароль короткий
+        strength = 'Weak'; 
         color = 'red';
     } else if (password.length >= 6 && !/\d/.test(password) && !/[^\w\s]/.test(password)) {
-        strength = 'Weak';  // Только буквы
+        strength = 'Weak';  
         color = 'red';
     } else if (password.length >= 6 && /\d/.test(password) && !/[^\w\s]/.test(password)) {
-        strength = 'Medium';  // Буквы и цифры
+        strength = 'Medium';  
         color = 'orange';
     } else if (password.length >= 6 && /\d/.test(password) && /[^\w\s]/.test(password)) {
-        strength = 'Strong';  // Буквы, цифры и специальные символы
+        strength = 'Strong';  
         color = 'green';
     }
 
@@ -52,17 +50,15 @@ passwordInput.addEventListener('input', () => {
 });
 
 passwordInput.addEventListener('blur', () => {
-    passwordStrength.style.display = 'none';  // Скрыть индикатор при потере фокуса
+    passwordStrength.style.display = 'none';  
 });
 
-// Сохранение данных пользователя в localStorage
 const registerForm = document.getElementById('registerForm');
 registerForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    // Проверка, что пароли совпадают
     const confirmPassword = document.getElementById('confirmPassword').value;
     if (password !== confirmPassword) {
         document.getElementById('registerMessage').textContent = 'Passwords do not match!';
